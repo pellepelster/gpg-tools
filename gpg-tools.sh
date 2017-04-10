7c69552dc48e5680c07f1b402673ef095ea06970
Christian Pelster <cpelster@thoughtworks.com>
2017-04-10 21:43:49 +0200

    filter mounted devices

--- gpg-tools.sh
+++ gpg-tools.sh
@@ -18,62 +18,73 @@ GPG_KEY_COMMENT=${GPG_KEY_COMMENT:-}
 GPG_KEY_PASSWORD=${GPG_KEY_PASSWORD:-}
 GPG_KEY_PASSWORD_RETYPE=${GPG_KEY_PASSWORD_RETYPE:-}
 
-declare -A DEVICE_IDS
+declare -A UNMOUNTED_DEVICE_IDS
 
-update_device_ids() {
+update_devices() {
   for device_link in $(find ${DEVICES_BASE_PATH} -name "usb-*"); do
-    local device_id=$(basename ${device_link}) 
+    local device_id=$(basename ${device_link})
     device_id=${device_id:4}
     device=$(readlink -f ${device_link})
-    DEVICE_IDS[${device}]=${device_id}
+    if [ $(is_mounted ${device}) -eq 0 ]; then
+      UNMOUNTED_DEVICE_IDS[${device}]=${device_id}
+    fi
   done
 }
 
 get_devices_menuitems() {
-  for device in "${!DEVICE_IDS[@]}"
+  for device in "${!UNMOUNTED_DEVICE_IDS[@]}"
   do
-    echo "${device} ${DEVICE_IDS[${device}]}"
+    echo "${device} ${UNMOUNTED_DEVICE_IDS[${device}]}"
   done
 }
 
 is_mounted() {
-  df | grep $1  2>&1 /dev/null
-  echo $?
+  echo $(df | grep $1 | wc -l)
 }
 
 mount_target_device() {
-    mount_path=$1
+  mount_path=$1
+  mount_name=$2
 
-    if [ ${#DEVICE_IDS[@]} -eq 0 ] ; then
-      dialog --msgbox "no usable devices found" 10 40
-      return
-    fi
+  if [ $(is_mounted ${mount_path}) -ne 0 ]; then
+    dialog --msgbox "Storage for ${mount_name} already mounted" 10 40
+    return
+  fi
 
-    dialog --backtitle "Device selection" \
-           --title "Select USB device to mount" \
-           --cancel-label "Cancel" \
-           --menu " " 17 60 10 \
-           $(get_devices_menuitems) 2> $ANSWER
-    opt=${?}
-    if [ $opt != 0 ]; then rm $ANSWER; exit; fi
-    device=$(cat $ANSWER)
-    case $device in
-        Quit) rm $ANSWER; exit;;
-        *)
-          if [ $(is_mounted ${device}) ] ; then
-            if sudo umount ${device} ; then
-              dialog --msgbox "failed to unmount ${device}" 10 78
-              return
-            fi
+  if [ ! -z ${UNMOUNTED_DEVICE_IDS+x} ] && [ ${#UNMOUNTED_DEVICE_IDS[@]} -eq 0 ]; then
+    dialog --msgbox "No usable devices found" 10 40
+    return
+  fi
+
+  dialog --backtitle "Device selection" \
+         --title "Select USB device to mount" \
+         --cancel-label "Cancel" \
+         --menu " " 17 60 10 \
+         $(get_devices_menuitems) 2> $ANSWER
+  opt=${?}
+  if [ $opt != 0 ]; then
+    rm $ANSWER
+    return
+  fi
+  device=$(cat $ANSWER)
+  case $device in
+      Quit) rm $ANSWER; exit;;
+      *)
+        if [ $(is_mounted ${device}) ]; then
+          if sudo umount ${device} ; then
+            dialog --msgbox "failed to unmount ${device}" 10 78
+            return
           fi
+        fi
 
-          if sudo mount ${device} ${mount_path} ; then
-            dialog --msgbox "mounted ${device} at ${mount_path}" 10 78
-          else
-            dialog --msgbox "failed to mount ${device} at ${mount_path}" 10 78
-          fi
-          ;;
-    esac
+        if sudo mount ${device} ${mount_path} ; then
+          dialog --msgbox "mounted ${device} at ${mount_path}" 10 78
+        else
+          dialog --msgbox "failed to mount ${device} at ${mount_path}" 10 78
+        fi
+        update_devices
+        ;;
+  esac
 }
 
 generate_master_key() {
@@ -92,7 +103,6 @@ cat >${TEMP_DIR}/master_key_script << EOF
      %echo done"
 EOF
 gpg --homedir ${TEMP_DIR} --batch --gen-key ${TEMP_DIR}/master_key_script
-	sleep 10
 }
 
 GPG_KEY_FORM_ERRORS=""
@@ -111,14 +121,14 @@ enter_master_key_data() {
         "Comment         :" 3 1 "${GPG_KEY_COMMENT}" 3 20 20 0 0 \
         "Password        :" 4 1 "${GPG_KEY_PASSWORD}" 4 20  20 0 1 \
         "Retype Password :" 5 1 "${GPG_KEY_PASSWORD_RETYPE}" 5 20  20 0 1 2> ${TEMP_DIR}/gpg_form_data.$$
-  if [ $? -eq 0 ] ; then
+  if [ $? -eq 0 ]; then
     form_data=( $(cat ${TEMP_DIR}/gpg_form_data.$$ | sed -e 's/^/=/') )
     GPG_KEY_REALNAME=${form_data[0]:1}
     GPG_KEY_EMAIL=${form_data[1]:1}
     GPG_KEY_COMMENT=${form_data[2]:1}
     GPG_KEY_PASSWORD=${form_data[3]:1}
     GPG_KEY_PASSWORD_RETYPE=${form_data[4]:1}
-    if [ $(validate_gpg_key_form_data) -ne 0 ] ; then
+    if [ $(validate_gpg_key_form_data) -ne 0 ]; then
       dialog --msgbox "${GPG_KEY_FORM_ERRORS}" 10 78
     fi
   fi
@@ -129,8 +139,8 @@ main_menu() {
            --title "Main Menu" \
            --cancel-label "Quit" \
            --menu "Move using [UP] [DOWN], [Enter] to select" 17 70 10\
-        MasterKeyStorage "Mount USB drive for master key storage"\
-        ExportKeyStorage "Mount USB drive for export key storage"\
+        Master "Mount USB drive for master key storage"\
+        Export "Mount USB drive for export key storage"\
         EnterMasterKeyData "Enter all data needed for key generation"\
         GenerateMasterKey "Generate a new master keypair"\
         Info "Show information"\
@@ -139,8 +149,8 @@ main_menu() {
     if [ $opt != 0 ]; then rm $ANSWER; exit; fi
     menuitem=$(cat $ANSWER)
     case $menuitem in
-        MasterKeyStorage) mount_target_device ${MASTER_KEYS_DIR};;
-        KeyStorage) mount_target_device ${EXPORT_KEYS_DIR};;
+        Master) mount_target_device ${MASTER_KEYS_DIR} "master keys";;
+        Export) mount_target_device ${EXPORT_KEYS_DIR} "exported keys";;
         GenerateMasterKey) generate_master_key;;
         EnterMasterKeyData) enter_master_key_data;;
         Info) information;;
@@ -151,22 +161,18 @@ main_menu() {
 
 cleanup_environment() {
   sync
-  sleep 1
-  sync
-  sleep 1
-  if [ $(is_mounted ${MASTER_KEYS_DIR}) -eq 0 ] ; then
+  if [ $(is_mounted ${MASTER_KEYS_DIR}) -ne 0 ]; then
     sudo umount -f ${MASTER_KEYS_DIR}
   fi
-  if [ $(is_mounted ${EXPORT_KEYS_DIR}) -eq 0 ] ; then
+  if [ $(is_mounted ${EXPORT_KEYS_DIR}) -ne 0 ]; then
     sudo umount -f ${EXPORT_KEYS_DIR}
   fi
-  sleep 1
   sudo umount -f ${TEMP_DIR}
   rm -rf ${TEMP_DIR}
 }
 trap cleanup_environment EXIT
 
-if [ -d ${TEMP_DIR} ] ; then
+if [ -d ${TEMP_DIR} ]; then
   echo "temp dir '${TEMP_DIR} already exists, aborting"
   exit 1
 else
@@ -181,7 +187,7 @@ fi
 
 
 while true; do
-  update_device_ids
+  update_devices
   main_menu
 done
 
